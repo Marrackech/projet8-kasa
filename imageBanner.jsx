5e48fb920b50aff5e52ce902f588026e7f4430dd
Marrackech <156908710+Marrackech@users.noreply.github.com>
2024-03-21 19:44:58 +0000

    Add files via upload
    ajuster le current picture index sur la pictures.length exp 1/6 2/6

--- imageBanner.jsx
+++ imageBanner.jsx
@@ -1,64 +1,56 @@
-import React from 'react'
-import { useState } from 'react';
-import "./imageBanner.css"
-function ImageBanner(props) {
-  //recuperer des elements pour le caroussel
-  const pictures = props.pictures;
 
+
+
+import React, { useState } from 'react';
+import "./imageBanner.css"
+
+function ImageBanner(props) {
+  const pictures = props.pictures;
   const [currentPicture, setCurrentPicture ] = useState(0);
   
   const getClassName = (i) => {
     if(i === currentPicture) return "show";
     return "";
   };
-  // click next et ne pas surpasser le length
+  
   const moveNext = () => {
-    setCurrentPicture((currentPicture + 1) % pictures.length)
-
-   }
-   const moveBack = () => {
-    const newCurrentPicture = currentPicture -1;
+    setCurrentPicture((currentPicture + 1) % pictures.length);
+  };
+  
+  const moveBack = () => {
+    const newCurrentPicture = currentPicture - 1;
     if(newCurrentPicture < 0) {
       setCurrentPicture(pictures.length - 1);
-     return;
-      
+      return;
     }
-    setCurrentPicture(currentPicture - 1)
-    
-   }
-   //afficher les bouttons seulements si pictures sont superieur a 1 
-   const picturesAvailable = () => {
-    return pictures && pictures.length > 1
-  }
-  console.log(picturesAvailable())
-  // afficher les images Caroussel
-   const getCaroussel = () => {
-    if (!picturesAvailable) {
-
- return false
+    setCurrentPicture(newCurrentPicture);
+  };
+  
+  const picturesAvailable = () => {
+    return pictures && pictures.length > 1;
+  };
+  
+  const getCaroussel = () => {
+    if (!pictures || pictures.length === 0) {
+      return null;
     }
-
-    return  pictures.map((pic, i) => ( <img key={pic} src={pic} alt="" className={getClassName(i)}/>));
-   
-     
-   }
+    return pictures.map((pic, i) => ( <img key={pic} src={pic} alt="" className={getClassName(i)}/>));
+  };
   
   return (
     <div className='banner'>
       <div className="container">
-        {/* appliquer la fonctions */}
-      {getCaroussel()}
-      {/* <img src={imageUrl} alt="" /> */}
-     
-</div>
-{/* appliquer la condition */}
-          {picturesAvailable() && <>
-          <button className='btn-next' onClick={moveNext}><i className='fas fa-chevron-left'/></button>
-      <button className='btn-back' onClick={moveBack}><i className='fas fa-chevron-right'/></button> 
-      </>  }
+        {getCaroussel()}
+      </div>
+      {picturesAvailable() && 
+        <>
+ <p className='numero'>{pictures.length - currentPicture}/{pictures.length}</p>
+          {pictures.length > 1 && <button className='btn-next' onClick={moveNext}><i className='fas fa-chevron-left'/></button>}
+          {pictures.length > 1 && <button className='btn-back' onClick={moveBack}><i className='fas fa-chevron-right'/></button>}
+        </>
+      }
     </div>
-  
   )
 }
 
-export default ImageBanner
+export default ImageBanner;
